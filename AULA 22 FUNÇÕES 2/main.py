02517daf8f687836d3cc45a1e7d867b09cafdb6e
felipepereiraqueirozzz-web <felipe.pereira.queirozzz@gmail.com>
2026-09-17 10:40:29 -0300

    Add files via upload

---
+++ AULA 22 FUNÇÕES 2/main.py
@@ -0,0 +1,10 @@
+def apresentar(nome, idade): # Parametro
+    print("Olá", nome, "sua idade é", idade)
+def somar(n1, n2):
+    return n1 + n2
+    
+apresentar("Adenilson", 78) # Argumento
+apresentar("João Antonio", 18)
+apresentar("Pereira", 15)
+
+
