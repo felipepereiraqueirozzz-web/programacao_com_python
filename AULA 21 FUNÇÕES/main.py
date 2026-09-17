0205d48b8684f82e275ba70690fe49736c258006
felipepereiraqueirozzz-web <felipe.pereira.queirozzz@gmail.com>
2026-09-17 10:40:11 -0300

    Add files via upload

---
+++ AULA 21 FUNÇÕES/main.py
@@ -0,0 +1,11 @@
+def calcular_media():
+    print(" Calculando a media")
+    
+def saudacao(nome):
+    print(f"Bem vindo! {nome}")
+    
+calcular_media()
+saudacao("Felipe")
+saudacao("Joao antonio")
+saudacao("Jonatan")
+saudacao("fefe")
